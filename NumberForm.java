fcb7ac389d4c0ce3c5b15b3828f22807d7b2b1d6
Ritesh786 <jharitesh6@gmail.com>
2016-08-03 14:53:22 +0530

    assignment 2
    assignment 2

---
+++ NumberForm.java
@@ -0,0 +1,23 @@
+package com.Ritesh.tools;
+
+import java.util.Scanner;
+
+public class NumberForm {
+
+	public static void main(String[] args) {
+		 int y;
+		 
+		 
+		 try{
+		 y=Integer.parseInt("xyz");
+		 System.out.println("" +y); }
+		 catch(NumberFormatException nf){
+			 System.out.println("Number format exception handled");
+		 }
+	
+		 
+		 
+		 
+		 }
+
+}
